857267502c588806100c9791c02af1b66bfd8612
BeyondNaxos <adopteuntrip@gmail.com>
2023-03-24 12:40:37 +0100

    update

--- components/alert.js
+++ components/alert.js
@@ -1,15 +1,15 @@
-import styles from "./alert.module.css"
-import {clsx} from "clsx"
+import styles from './alert.module.css'
+import { clsx } from 'clsx'
 
-export default function Alert({children, type}) {
-    return (
-        <div
-            className={clsx({
-                [styles.success]: type === "success",
-                [styles.error]: type === "error",
-            })}
-            >
-                {children}
-            </div>
-    )
+export default function Alert({ children, type }) {
+  return (
+    <div
+      className={clsx({
+        [styles.success]: type === 'success',
+        [styles.error]: type === 'error',
+      })}
+    >
+      {children}
+    </div>
+  )
 }
